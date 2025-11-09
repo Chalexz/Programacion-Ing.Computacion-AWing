b9199839b2e20236bcc601a88d0ed84b2ce17a99
AlexW <alexwingtablet@gmail.com>
2025-11-09 15:18:47 -0600

    menu principal

--- Taller de programación_S2-2025/Proyecto_2/README.txt
+++
@@ -1,3 +0,0 @@
-No se puede usar funciones integradas para las listas y matrices. Para lo demás, me parece que sí.
-
-
--- Taller de programación_S2-2025/Proyecto_2/main.py
+++ Taller de programación_S2-2025/Proyecto_2/main.py
@@ -1,8 +1,5 @@
-#Label, Button, Entry, Text, Frame, Checkbutton, Radiobutton, 
-# Listbox, Scale, Spinbox, Canvas, Menu, Toplevel.
-
 '''
-SUPER PROYECTO HERMANO MAURICIO
+PROYECTO SUPER HERMANO MAURICIO
 
 TALLER DE PROGRAMACION | S2-2025
 
@@ -12,10 +9,45 @@ POR: ALEXANDER WING
 '''
 
 import tkinter as tk
-from tkinter import ttk
 
-root = tk.Tk()
+#==============================================( MENU PRINCIPAL )==============================================================#
+
+root = tk.Tk() 
 root.title("Hermano Mauricio")
-root.geometry("400x250")
-root.mainloop
+root.geometry("700x600")
+
+
+menu_principal = tk.Frame(root, bg="#000000")
+menu_principal.pack(fill="both", expand=True)
+
+menu_principal.columnconfigure(0, weight=1)
+menu_principal.rowconfigure(0, weight=1)
+menu_principal.rowconfigure(1, weight=1)
+
+titulo_menu_principal = tk.Label(menu_principal, text="ᗒ MENU PRINCIPAL ᗕ"
+                                 , fg="white", bg="#000000", font=("Roboto", 18, "bold"))
+titulo_menu_principal.grid(row=0, rowspan=2, column=0, sticky="ew")
+
+#==============================================BOTON INICIAR JUEGOs
+
+btn_iniciar_juego = tk.Button(menu_principal, text="INICIAR JUEGO" )
+btn_iniciar_juego.grid(row=2, column=0,padx=50, pady=70, sticky="nsew")
+btn_iniciar_juego.configure(bg="#3A3636", fg="white", font="Roboto",
+                            activebackground="#4B4747", activeforeground="white",
+                            highlightthickness=0)
+
+#==============================================BOTON VER RANKING
+
+btn_ver_ranking = tk.Button(menu_principal, text="VER RANKING")
+btn_ver_ranking.grid(column=0, row=3, padx=50, pady=70, sticky="nsew")
+btn_ver_ranking.configure(bg="#3A3636", fg="white", font="Roboto",
+                          activebackground="#4B4747", activeforeground="white",
+                          highlightthickness=0)
+
+root.mainloop()
+#==============================================( MENU PRINCIPAL )==============================================================#
+
+
+
+
 
