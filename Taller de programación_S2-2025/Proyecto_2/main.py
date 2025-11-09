7c01d84a28f02de199e13bd9be964e5b0a074aae
AlexW <alexwingtablet@gmail.com>
2025-11-09 13:24:20 -0600

    primera prueba

---
+++ Taller de programación_S2-2025/Proyecto_2/main.py
@@ -0,0 +1,21 @@
+#Label, Button, Entry, Text, Frame, Checkbutton, Radiobutton, 
+# Listbox, Scale, Spinbox, Canvas, Menu, Toplevel.
+
+'''
+SUPER PROYECTO HERMANO MAURICIO
+
+TALLER DE PROGRAMACION | S2-2025
+
+POR: ALEXANDER WING
+
+╭∩╮(-_-)╭∩╮
+'''
+
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+root.title("Hermano Mauricio")
+root.geometry("400x250")
+root.mainloop
+
